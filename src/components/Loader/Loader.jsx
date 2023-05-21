ce9067f9dbb994a9d3b51b4c3b64499f0e6a4132
Iuliia Sokolovska <uu.sokil@gmail.com>
2023-05-21 18:54:08 +0200

    fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -11,7 +11,7 @@ export function Loader() {
       alignItems="center"
       width="100%"
       height="100%"
-      margin=" 0 auto"
+      margin="0 auto"
     >
       <RotatingLines
         strokeColor="grey"
